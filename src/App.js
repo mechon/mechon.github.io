7953ff93eedbd4526a001c3a79c75cba686ff7e9
mechon <mark.c.echon@gmail.com>
2022-10-11 22:13:20 +0800

    Add react worspace branch

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './doggo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>SITE UNDER CONSTRUCTION</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
